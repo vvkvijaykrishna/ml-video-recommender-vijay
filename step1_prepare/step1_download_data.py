8e880a93e5b22bd1d5f3d75824d3d1deb7c0f164
Vijay Krishna <vj4games@gmail.com>
2025-08-10 19:25:43 -0400

    changed step1_download_data.py to read data from all student ids

--- step1_prepare/step1_download_data.py
+++ step1_prepare/step1_download_data.py
@@ -9,8 +9,23 @@ print(os.path.basename(__file__), 'videos_dataframe: \n{}'.format(videos_datafra
 videos_dataframe.to_csv('step1_videos.csv', index=False)
 
 # Read the learning events CSV into a DataFrame, and write the DataFrame to a CSV file
-video_learning_events_csv_url = 'https://raw.githubusercontent.com/elimu-ai/ml-datasets/main/lang-ENG/video-learning-events.csv'
-print(os.path.basename(__file__), f'video_learning_events_csv_url: {video_learning_events_csv_url}')
-video_learning_events_dataframe = pandas.read_csv(video_learning_events_csv_url)
-print(os.path.basename(__file__), f'video_learning_events_dataframe: \n{video_learning_events_dataframe}')
-video_learning_events_dataframe.to_csv('step1_video_learning_events.csv', index=False)
+# video_learning_events_csv_url = 'https://raw.githubusercontent.com/elimu-ai/ml-datasets/main/lang-ENG/video-learning-events.csv'
+# print(os.path.basename(__file__), f'video_learning_events_csv_url: {video_learning_events_csv_url}')
+# video_learning_events_dataframe = pandas.read_csv(video_learning_events_csv_url)
+# print(os.path.basename(__file__), f'video_learning_events_dataframe: \n{video_learning_events_dataframe}')
+# video_learning_events_dataframe.to_csv('step1_video_learning_events.csv', index=False)
+
+with open('step1_video_learning_events.csv', 'w') as f:
+    pass
+
+students_url = 'https://raw.githubusercontent.com/elimu-ai/ml-datasets/main/lang-ENG/students.csv'
+print(os.path.basename(__file__), 'students_url: {}'.format(students_url))
+students_dataframe = pandas.read_csv(students_url)
+print(os.path.basename(__file__), 'students_dataframe: \n{}'.format(students_dataframe))
+student_ids = students_dataframe['id'].copy()
+for student_id in student_ids:
+    video_learning_events_csv_url = f"https://raw.githubusercontent.com/elimu-ai/ml-datasets/main/lang-ENG/student-id-{student_id}/video-learning-events.csv"
+    print(f"URL: {video_learning_events_csv_url}")
+    video_learning_events_dataframe = pandas.read_csv(video_learning_events_csv_url)
+    print(os.path.basename(__file__), 'video_learning_events_dataframe: \n{}'.format(video_learning_events_dataframe))
+    video_learning_events_dataframe.to_csv('step1_video_learning_events.csv', mode='a', index=False, header=False)
