7e2797d5ceb78cb0f61ed72d138b30dac2827f09
Vijay Krishna <vj4games@gmail.com>
2025-08-11 18:24:41 -0400

    adding column titles to step1_video_learning_events.csv

--- step1_prepare/step1_download_data.py
+++ step1_prepare/step1_download_data.py
@@ -15,7 +15,12 @@ print(os.path.basename(__file__), 'students_url: {}'.format(students_url))
 students_dataframe = pandas.read_csv(students_url)
 print(os.path.basename(__file__), 'students_dataframe: \n{}'.format(students_dataframe))
 student_ids = students_dataframe['id'].copy()
-for student_id in student_ids:
+
+if not student_ids.empty:
+    video_learning_events_csv_url = f"https://raw.githubusercontent.com/elimu-ai/ml-datasets/main/lang-ENG/student-id-{student_ids[0]}/video-learning-events.csv"
+    video_learning_events_dataframe = pandas.read_csv(video_learning_events_csv_url)
+    video_learning_events_dataframe.to_csv('step1_video_learning_events.csv', index=False)
+for student_id in student_ids[1:]:
     video_learning_events_csv_url = f"https://raw.githubusercontent.com/elimu-ai/ml-datasets/main/lang-ENG/student-id-{student_id}/video-learning-events.csv"
     print(f"URL: {video_learning_events_csv_url}")
     video_learning_events_dataframe = pandas.read_csv(video_learning_events_csv_url)
